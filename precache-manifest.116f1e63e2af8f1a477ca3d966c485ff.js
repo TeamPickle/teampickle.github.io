e71f15b8a81ecee8bdc66b1bb6ab367972c78547
CraftyDragon678 <craftydragon678@gmail.com>
2020-04-08 20:30:52 +0900

    init: add test build files

---
+++ precache-manifest.116f1e63e2af8f1a477ca3d966c485ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31d52c8f5fa3aa2507adefa2f4e2f000",
+    "url": "/index.html"
+  },
+  {
+    "revision": "468912cfcffc7d73e7ef",
+    "url": "/static/js/2.1689c35a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1689c35a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35210363f1658ab79e7d",
+    "url": "/static/js/main.3301a515.chunk.js"
+  },
+  {
+    "revision": "76c67e1acb7ae6338838",
+    "url": "/static/js/runtime-main.aa75c326.js"
+  }
+]);
